8e3c6391e6f73009f5fff15445bbde52afb9a150
rullah0202 <rullah0202@gmail.com>
2022-03-14 04:47:36 -0700

    Laravel Portfolio Website
    Laravel Portfolio Website

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\VisitorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('Home');
-});
+Route::get('/', [HomeController::class,'HomeIndex']);
+Route::get('/visitor', [VisitorController::class,'VisitorIndex']);
